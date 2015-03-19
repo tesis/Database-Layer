c1c89acdda760cddf716699eec45b92751ef0295
Tereza Simcic <tereza.simcic@gmail.com>
2015-03-19 21:34:12 +0100

    Removed config file

--- app/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('DB_HOST') ? null : define ('DB_HOST', 'localhost');
-defined('DB_USER') ? null : define ('DB_USER', 'root');
-defined('DB_PASS') ? null : define ('DB_PASS', 'tesi');
-defined('DB_NAME') ? null : define ('DB_NAME', 'pdoTest');
-defined('DB_CHARSET') ? null : define ('DB_CHARSET', 'utf8');
